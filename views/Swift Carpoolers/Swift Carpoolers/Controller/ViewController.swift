3f106b45d9af709836db905f46794257b4afbc73
Timmy Lee <timmylee.dev@gmail.com>
2018-11-09 08:23:18 -0800

    Update request to accomodate post

--- views/Swift Carpoolers/Swift Carpoolers/Controller/ViewController.swift
+++ views/Swift Carpoolers/Swift Carpoolers/Controller/ViewController.swift
@@ -27,19 +27,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var errorLabel: UILabel!
     
     @IBAction func register(_ sender: Any) {
+//        let requestBody: [String: String] = [
+//            "user_id": username.text!,
+//            "first_name": firstName.text!,
+//            "last_name": lastName.text!,
+//            "password": password.text!,
+//            "phone_number": phoneNumber.text!
+//        ]
+        
         let requestBody: [String: String] = [
-            "username": username.text!,
-            "firstName": firstName.text!,
-            "lastName": lastName.text!,
-            "password": password.text!,
-            "phoneNumber": phoneNumber.text!
+            "user_id": "tilee212234@marketo.com",
+            "first_name": "firstName.text!",
+            "last_name": "lastName.text!",
+            "password": "password.text!",
+            "phone_number": "phoneNumber.text!",
+            "gender": "placeholder"
         ]
         
-        let response = makeRequest("GET", "https://api.github.com/search/users?q=timmy-lee", requestBody)
+        let response = makeRequest("POST", "http://35.235.94.177:8000/users/reg/", requestBody)
         
         print(response)
         
-        if let responseStatus = response["status"] as? Bool, responseStatus {
+        if let responseStatus = response["success"] as? Bool, responseStatus {
             print("Successful")
         } else {
             print("Unsuccessful")
@@ -73,6 +82,8 @@ class ViewController: UIViewController {
         if (method != "GET") {
             do {
                 request.httpBody = try JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
+                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                request.addValue("application/json", forHTTPHeaderField: "Accept")
             } catch let error {
                 return ["error": error]
             }
@@ -93,7 +104,11 @@ class ViewController: UIViewController {
             }
             
             do {
+//                print("Raw response = \(responseData)")
+//                print(String(data: responseData, encoding: String.Encoding.utf8))
+//                let httpReponse = responseData as? HTTPURLResponse
                 let responseJson = try JSONSerialization.jsonObject(with: responseData, options: [])
+                print(responseJson)
                 guard let castedResponse = responseJson as? [String: Any] else { return }
 //                guard let responseArray = responseJson as? [[String: Any]] else { return } // Assuming response is an array
                 semaphore.signal()
