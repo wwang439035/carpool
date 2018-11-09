9b4bbeb1d3c7e42a79abe970474745cb4055822a
Timmy Lee <timmylee.dev@gmail.com>
2018-11-09 13:41:12 -0800

    Full api flow

--- views/Swift Carpoolers/Swift Carpoolers/Controller/ViewController.swift
+++ views/Swift Carpoolers/Swift Carpoolers/Controller/ViewController.swift
@@ -9,25 +9,31 @@
 import UIKit
 import UserNotifications
 
-class ViewController: UIViewController {
-    
-//    var username : String = "";
-//    var firstName : String = "";
-//    var lastName : String = "";
-//    var password : String = "";
-//    var phoneNumber : String = "";
+var userData : [String: Any] = [String: Any]()
 
+class ViewController: UIViewController {
+    // Registration
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var firstName: UITextField!
     @IBOutlet weak var lastName: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var phoneNumber: UITextField!
     @IBOutlet weak var registerErrorLabel: UILabel!
-    
+
+    // Login
     @IBOutlet weak var emailAddressText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var errorLabel: UILabel!
-    
+
+    // Preferences
+    @IBOutlet weak var driver: UISegmentedControl!
+    @IBOutlet weak var gender: UITextField!
+    @IBOutlet weak var currentLocation: UITextField!
+    @IBOutlet weak var destination: UITextField!
+    @IBOutlet weak var firstStop: UITextField!
+    @IBOutlet weak var secondStop: UITextField!
+    @IBOutlet weak var preferredTime: UIDatePicker!
+
     @IBAction func register(_ sender: Any) {
         let requestBody: [String: String] = [
             "user_id": username.text!,
@@ -37,27 +43,42 @@ class ViewController: UIViewController {
             "phone_number": phoneNumber.text!,
             "gender": "placeholder"
         ]
-        if(validateEmailAddress(email: username.text ?? "") && firstName.hasText && lastName.hasText && password.hasText){
+//        let requestBody: [String: String] = [
+//            "user_id": "hello3214ew2342@gmail.com",
+//            "first_name": "test",
+//            "last_name": "test",
+//            "password": "test",
+//            "phone_number": "777777777",
+//            "gender": "placeholder"
+//        ]
+
+        userData["user_id"] = username.text!
+        userData["first_name"] = firstName.text!
+        userData["last_name"] = lastName.text!
+        userData["password"] = password.text!
+        userData["phone_number"] = phoneNumber.text!
+
+        if (validateEmailAddress(email: username.text ?? "") && firstName.hasText && lastName.hasText && password.hasText) {
             registerErrorLabel.isHidden = true
-            
+
             //api call
             let response = makeRequest("POST", "http://35.235.94.177:8000/users/reg/", requestBody)
-            
+
             print(response)
-            
+
             if let responseStatus = response["success"] as? Bool, responseStatus {
                 performSegue(withIdentifier: "goToPreferencesSegue", sender: nil)
                 print("Successful")
             } else {
                 print("Unsuccessful")
             }
-            
+
         } else {
             registerErrorLabel.isHidden = false
         }
-        
+
     }
-    
+
     @IBAction func LoginButton(_ sender: UIButton) {
         if(validateEmailAddress(email: emailAddressText.text ?? "") && passwordText.hasText){
             errorLabel.isHidden = true
@@ -66,25 +87,58 @@ class ViewController: UIViewController {
             errorLabel.isHidden = false
         }
     }
-    
+
+    @IBAction func savePreferences(_ sender: Any) {
+        print("saving preferences")
+        let requestBody: [String: Any] = [
+            "type": driver.titleForSegment(at: driver.selectedSegmentIndex)! == "Share Ride" ? 0 : 1,
+            "id": "20",
+            "dest": destination.text!,
+            "gender": gender.text!
+        ]
+
+        userData["type"] = requestBody["type"]
+        userData["dest"] = requestBody["dest"]
+        userData["gender"] = requestBody["gender"]
+    }
+
+    @IBAction func scheduleTime(_ sender: UIButton) {
+        print("Inside scheduleTime")
+        print(preferredTime.date)
+
+        let requestBody: [String: Any?] = [
+            "type": userData["type"],
+            "id": 20,
+            "dest": userData["dest"],
+            "gender": userData["gender"],
+            "time_window": preferredTime.date
+        ]
+
+        print(userData)
+
+//        let response = makeRequest("POST", "http://35.235.94.177:8000/schedule/save.json", requestBody)
+
+//        print(response)
+    }
+
     func validateEmailAddress(email: String) -> Bool{
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        
+
         return emailTest.evaluate(with: email)
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         if(errorLabel != nil){
            errorLabel.isHidden = true
         }
-        
+
         if(registerErrorLabel != nil){
             registerErrorLabel.isHidden = true
         }
     }
-    
+
     override func shouldPerformSegue(withIdentifier identifier: String?, sender: Any?) -> Bool {
         if let ident = identifier {
             if ident == "unsuccessfulRegistration" {
@@ -94,8 +148,8 @@ class ViewController: UIViewController {
         }
         return true
     }
-    
-    func makeRequest(_ method: String, _ url: String, _ params: [String: String]) -> Dictionary<String, Any> {
+
+    func makeRequest(_ method: String, _ url: String, _ params: [String: Any?]) -> Dictionary<String, Any> {
         var request = URLRequest(url: URL(string: url)!)
         let semaphore = DispatchSemaphore(value: 0)
         request.httpMethod = method
@@ -110,7 +164,7 @@ class ViewController: UIViewController {
             }
         }
         let session = URLSession.shared
-        
+
         var responseDict : [String: Any] = [String: Any]()
         let task = session.dataTask(with: request) {
             (data, response, error) in
@@ -118,12 +172,12 @@ class ViewController: UIViewController {
                 print("error ")
                 return
             }
-            
+
             guard let responseData = data else {
                 print("Error: did not receive data")
-                return 
+                return
             }
-            
+
             do {
 //                print("Raw response = \(responseData)")
 //                print(String(data: responseData, encoding: String.Encoding.utf8))
@@ -142,11 +196,11 @@ class ViewController: UIViewController {
 
         task.resume()
         semaphore.wait()
-        
+
         return responseDict
     }
-    
-    
+
+
     @IBAction func NotifyMeButton(_ sender: UIButton) {
 //        let content = UNMutableNotificationContent()
 //        content.title = "Weekly Staff Meeting"
@@ -173,7 +227,6 @@ class ViewController: UIViewController {
             self.performSegue(withIdentifier: "bestMatchSegue", sender: nil)
             }
         }
-    
-    
-}
 
+
+}
